1d6010578e5388fc71c6b4f405441a1935662e47
kzkmz01 <tweenhills@hotmail.co.uk>
2021-09-29 18:13:07 +0100

    Create oauth.js

---
+++ oauth.js
@@ -0,0 +1,49 @@
+const x = selector => document.querySelector(selector);
+
+const processEntries = () => {
+  console.log("processEntries function has started");
+
+  var url = "https://w373dhtpiaexl2b-genis.adb.uk-london-1.oraclecloudapps.com/ords/oc_admin/oauth/token";
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", url);
+
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.setRequestHeader("Authorization", "Basic cklidUZWZXJ6cmRGUE5YWlNRN2hsdy4uOkdELXMyRmw5S0NfSE1TU0R1X2psZ3cuLg==");
+
+  var data = "grant_type=client_credentials";
+
+  xhr.send(data);
+
+  xhr.onreadystatechange = function () {
+     if (xhr.readyState === 4) {
+        var data=xhr.responseText;
+        var jsonResponse = JSON.parse(data);
+        var acc_token = jsonResponse["access_token"]
+
+      var email = document.getElementById("email").value;
+      console.log(email);
+
+      
+      var settings = {
+        "url": "https://w373dhtpiaexl2b-genis.adb.uk-london-1.oraclecloudapps.com/ords/oc_admin/members/check_email/" + email,
+        "method": "GET",
+        "timeout": 0,
+        "headers": {
+          "Authorization": "Bearer " + acc_token
+        },
+      };
+      
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+      });
+
+    }};
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  x("#get_data").addEventListener("click", processEntries);
+  x("#email").focus();
+});
+
+
